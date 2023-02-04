fa43125d6e4f6729f65180619240ce5b47084956
Ben Lerchin <ben@lerchin.com>
2023-02-04 02:30:25 +0000

    use more sensible header parsing

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const fs = require('fs');
+const readline = require('readline');
 const assert = require('assert');
 const jpeg = require('jpeg-js');
 
 const CHUNK_SIZE = 1024;
 const OUT_SIZE = 8192;
 
-const ppmIn = process.argv[2];
-const ppm = fs.createReadStream(ppmIn);
+const ppmIn = process.argv[2]; const ppm = fs.createReadStream(ppmIn);
 const out = process.argv[3];
 
 let numOut = 0;
@@ -17,12 +17,8 @@ let bufs = [];
 
 ppm.on('readable', function() {
   let chunk;
-  let remain;
-  if (!srcWidth) {
-    chunk = ppm.read(CHUNK_SIZE);
-    remain = readHeader(chunk);
-    ppm.unshift(remain);
-    ppm.read(1270);
+  if(!srcWidth) {
+    readHeader(ppm);
   }
   while (null !== (chunk = ppm.read(srcWidth * 3))) {
     const alphaChunk = new Buffer(srcWidth * 4);
@@ -37,43 +33,47 @@ ppm.on('readable', function() {
       writeFile();
     }
   }
+  //writeFile();
 });
 
 function writeFile() {
+  console.log("writing " + bufs.length + " lines")
   const raster = Buffer.concat(bufs);
   const img = jpeg.encode({
     data: raster,
     height: OUT_SIZE,
-    width: srcWidth
+    width: bufs.length 
   });
   fs.writeFileSync(out + '--' + numOut + '.jpg', img.data);
   bufs = [];
   numOut++;
+  console.log("wrote lines");
 }
 
 
 
-function readHeader(buf) {
-  let lines = buf.toString('utf-8').split('\n');
-  let line = 0;
-  let max = 0;
-  function next() {
-    line++;
-  }
-  while (!max && line < 12) {
-    if (lines[line][0] === '#') {
-      next();
-    } else if (lines[line] === 'P6') {
-      next();
+function readHeader(stream) {
+  let line = "";
+  let max = false;
+  while (!max) {
+    line = ""
+    let char;
+    while(char != '\n') {
+      char = stream.read(1).toString();
+      line += char;
+    }
+    if (line[0] === '#') {
+      continue;
+    } else if (line === 'P6') {
+      continue;
     } else if (!srcWidth) {
-      srcWidth = parseInt(lines[line].split(' ')[0]);
-      next();
-    } else if (lines[line] == '65535') {
+      srcWidth = parseInt(line.split(' ')[0]);
+      continue;
+    } else if (line.includes('65535')) {
       max = true;
-      next();
     }
   }
   assert.notEqual(srcWidth, 0);
   assert.equal(max, true);
-  return Buffer.from(lines.slice(line + 1).join('\n'), 'utf-8');
+  console.log('width: ', srcWidth);
 }
